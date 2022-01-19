a0bacdf1f725fd19284ff99b72d983770abf6b03
REI <re.i.07260000@gmail.com>
2022-01-19 14:26:20 +0900

    commit

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -1,73 +1,37 @@
 <?php
+
+
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Author;
 
-class AuthorController extends Controller
-{
+class AuthorController extends Controller {
     public function index()
     {
-        $items = Author::all();
-        return view('index', ['items' => $items]);
+        $user = Auth::user();
+        $items = Author::paginate(4);
+        $param = ['items' => $items, 'user' =>$user];
+        return view('index', $param);
     }
-    public function find()
+
+    public function check(Request $request)
     {
-        return view('find', ['input' => '']);
+    $text = ['text' => 'ログインして下さい。'];
+    return view('auth', $text);
     }
-    public function search(Request $request)
+
+    public function checkUser(Request $request)
     {
-        $item = Author::where('name', 'LIKE',"%{$request->input}%")->first();
-        $param = [
-            'input' => $request->input,
-            'item' => $item
-        ];
-        return view('find', $param);
+    $email = $request->email;
+    $password = $request->password;
+    if (Auth::attempt(['email' => $email,
+            'password' => $password])) {
+        $text =   Auth::user()->name . 'さんがログインしました';
+    } else {
+        $text = 'ログインに失敗しました';
     }
-    public function bind(Author $author)
-    {
-        $data = [
-            'item'=>$author,
-        ];
-        return view('author.binds', $data);
+    return view('auth', ['text' => $text]);
     }
-    public function add()
-    {
-        return view('add');
-    }
-    public function create(Request $request)
-    {
-        $this->validate($request, Author::$rules);
-        $form = $request->all();
-        Author::create($form);
-        return redirect('/');
-    }
-    public function edit(Request $request)
-    {
-        $author = Author::find($request->id);
-        return view('edit', ['form' => $author]);
-    }
-    public function update(Request $request)
-    {
-        $this->validate($request, Author::$rules);
-        $form = $request->all();
-        unset($form['_token']);
-        Author::where('id', $request->id)->update($form);
-        return redirect('/');
-    }
-    public function delete(Request $request)
-    {
-        $author = Author::find($request->id);
-        return view('delete', ['form' => $author]);
-    }
-    public function remove(Request $request)
-    {
-        Author::find($request->id)->delete();
-        return redirect('/');
-    }
-    public function relate(Request $request) //追記
-    {
-        $items = Author::all();
-        return view('author.index', ['items' => $items]);
-    }
-}
+} 
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\SessionController;
+use App\Models\Person; 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,31 +17,43 @@ use App\Http\Controllers\SessionController;
 |
 */
 
+Route::get('/home', [AuthorController::class, 'index']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
+Route::get('/edit', [AuthorController::class, 'edit']);
+Route::post('/edit', [AuthorController::class, 'update']);
+Route::get('/delete', [AuthorController::class, 'delete']);
+Route::post('/delete', [AuthorController::class, 'remove']);
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
+Route::get('/author/{author}',[AuthorController::class, 'bind']);
+Route::prefix('book')->group(function () {
+  Route::get('/', [BookController::class, 'index']);
+  Route::get('/add', [BookController::class, 'add']);
+  Route::post('/add', [BookController::class, 'create']);
+});
+Route::get('/relation', [AuthorController::class, 'relate']);
+
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*laraver入門問題
-Route::get('/building/{room?}', [TestController::class, 'index']);
-*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
+Route::get('/auth', [AuthorController::class,'check']);
+Route::post('/auth', [AuthorController::class,'checkUser']);
 
-Route::prefix('book')->group(function () {//以下を追記
-    Route::get('/', [BookController::class, 'index']);
-    Route::get('/add', [BookController::class, 'add']);
-    Route::post('/add', [BookController::class, 'create']);
-});
-Route::get('/relation', [AuthorController::class, 'relate']);
-Route::get('/session', [SessionController::class, 'getSes']);
-Route::post('/session', [SessionController::class, 'postSes']);
-
-/*Routes問題１
-Route::get('/test/{room}/{id}', function($room,$id) {
-    return 'roomが'.$room .'でidは'.$id.'です';
+Route::get('/softdelete', function () {
+    Person::find(1)->delete();
 });
 
-//Routes問題２
-Route::get('/test/{Bonjour?}',function($Bonjour='Goodmorning'){
-    return $Bonjour.'=おはようございます';
+Route::get('softdelete/get', function() {
+    $person = Person::onlyTrashed()->get();
+    dd($person);
 });
-*/
